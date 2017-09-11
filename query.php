8a6f2adb5cbebb342359133fc72af5c7c534f2da
Long Cheng <296245482@qq.com>
2017-09-11 16:32:21 +0800

    Function added

--- query.php
+++ query.php
@@ -1,74 +1,104 @@
 <?php
+
+if(file_exists('export.xls')){
+    unlink('export.xls');
+}
+
+
 $connection = @mysqli_connect("127.0.0.1","root","root");
 mysqli_select_db($connection,"pm25");
 $user=$_POST["UserId"];
 $sday=$_POST["SDay"];
 $eday=$_POST["EDay"];
+$days = ((strtotime($eday) - strtotime($sday))/86400 + 1);
 
-echo "This is the result for " . $user . " from " . $sday . " to " . $eday . "<br>";
-echo "Total " . ((strtotime($eday) - strtotime($sday))/86400 + 1) . " days in our results";
+echo "Here is the result for " . $user . " from " . $sday . " to " . $eday . "<br>";
+echo "Total " . $days . " days in our results<br>";
 
-echo "<form class=\"form-start\" action=\"query.html\"><button type=\"submit\" id=\"return\">return</button></form>";
-
-
-$sday=$sday . ' 00:00:00';
-$eday=$eday . ' 23:59:59';
-
-$sqlstr1 = "Select * from data_mobile_new where userid=$user and time_point >= '$sday' and time_point <= '$eday' order by time_point desc;";
-$sqlstr2 = "show columns from data_mobile_new;";
-
-$result1=mysqli_query($connection, $sqlstr1);
-
-$columns = 0;
-$index = 0;
-$data = array();
+$userId="";
+$idsql = "SELECT id FROM pm25.user where name = '$user';";
+$idResult = mysqli_query($connection, $idsql);
+while($row = mysqli_fetch_array($idResult)){
+    $userId = $row['id'];
+}
 
 
+if($userId){
+    $startDay=$sday . ' 00:00:00';
+    $endDay=$eday . ' 23:59:59';
+    $sqlstr1 = "Select * from data_mobile_new where userid=$userId and time_point >= '$startDay' and time_point <= '$endDay' order by time_point desc;";
+    $sqlstr2 = "show columns from data_mobile_new;";
 
-if(!$result1){
-    echo "No Data found";
-}else{
+    $result1=mysqli_query($connection, $sqlstr1);
+    $result_num=mysqli_num_rows($result1);
 
-    echo "<table id='data' ><tr>";
+    echo $result_num . " records found, about " . number_format(($result_num)/(7.2*$days), 2) . "% data was recorded in these days";
+    echo "<br><form class=\"form-start\" action=\"query.html\"><button type=\"submit\" id=\"return\">return</button></form>";
 
-    $field = mysqli_query(@$connection, $sqlstr2);
-    while($row = mysqli_fetch_array($field)){
-        echo "<th>" . $row['Field'] . "</th>";
-        $columns += 1;
+    echo "<form method=\"get\" action=\"export.xls\"><button type=\"submit\">Download the Excel File!</button></form>";
+
+    $columns = 0;
+    $index = 0;
+    $data = array();
+
+
+    require_once('./Classes/PHPExcel.php');
+    $excel = new PHPExcel();
+    $letter = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q');
+    $tableheader = array('id', 'userid', 'database_access_token', 'time_point', 'longitude', 'latitude', 'outdoor', 'status', 'steps', 'heart_rate', 'ventilation_rate', 'ventilation_vol', 'pm25_concen', 'pm25_intake', 'pm25_datasource', 'pm25_monitor', 'APP_version
+');
+    for($i = 0; $i < count($tableheader); $i++) {
+        $excel->getActiveSheet()->setCellValue("$letter[$i]1","$tableheader[$i]");
     }
 
-    echo "</tr>";
+    if($result_num){
+        echo "result found";
+        echo "<table id='data' ><tr>";
 
-    while($row = mysqli_fetch_assoc($result1)){
-        foreach ( $row as $key => $val){
-            $data[$index] = $val;
-            $index += 1;
-        }
-    }
-
-    $subIndex = 0;
-
-    for($i = 0; $i < count($data);) {
-        echo "<tr>";
-
-        for($j = 0; $j < $columns; $j++) {
-            echo "<td>";
-
-            echo $data[$subIndex];
-            $subIndex += 1;
-            $i++;
-
-            echo "</td>";
+        $field = mysqli_query(@$connection, $sqlstr2);
+        while($row = mysqli_fetch_array($field)){
+            echo "<th>" . $row['Field'] . "</th>";
+            $columns += 1;
         }
 
         echo "</tr>";
 
+        while($row = mysqli_fetch_assoc($result1)){
+            foreach ( $row as $key => $val){
+                $data[$index] = $val;
+                $index += 1;
+            }
+        }
+
+        $subIndex = 0;
+        $excelRowIndex = 2;
+        for($i = 0; $i < count($data);) {
+            echo "<tr>";
+            $excelIndex = 0;
+            for($j = 0; $j < $columns; $j++) {
+                echo "<td>";
+                echo $data[$subIndex];
+                $excel->getActiveSheet()->setCellValue("$letter[$excelIndex]$excelRowIndex","$data[$subIndex]");
+                $subIndex += 1;
+                $i++;
+                echo "</td>";
+                $excelIndex ++;
+            }
+            $excelRowIndex ++;
+            echo "</tr>";
+        }
+    }else{
+        echo "No Data found";
     }
+
+    $write=new PHPExcel_Writer_Excel5($excel);
+    $write->save('export.xls');
+
+    echo "</table>";
+}else{
+    echo "Didn't find this user";
 }
 
-
-
-echo "</table>";
 ?>
 
 
